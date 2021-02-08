90f62a4ebcef03a734952f68a5f16db8d5caf628
Anil_Ramcharran <anil.gitme@gmail.com>
2021-02-08 00:19:51 -0500

    testing code grade

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_DATA_START = 'FETCH_DATA_START';
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
 export const FETCH_DATA_FAILED = 'FETCH_DATA_FAILED';
-//posting
+///posting
 export const POST_DATA_START = 'POST_DATA_START';
 export const POST_DATA_SUCCESS = 'POST_DATA_SUCCESS';
 export const POST_DATA_FAILED = 'POST_SMURF_FAILED'
